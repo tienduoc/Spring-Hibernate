070a49c427e5d8ca1a10429310d65557779be7ab
bl@ck_c@t <tienduoc@users.noreply.github.com>
2018-04-14 11:35:09 +0700

    038-039: DI - Injecting Literal Values

--- spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -16,6 +16,10 @@ public class SetterDemoApp {
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
 		
+		// call our new methods to get the literal values
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
+		
 		// close the context
 		context.close();
 	}
