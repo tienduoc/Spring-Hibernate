d4d9429312d91e4ed07db4c6cf8762133d5d83b6
bl@ck_c@t <tienduoc@users.noreply.github.com>
2018-04-14 22:36:16 +0700

    047-049: Bean Lifecycle init-method destroy-method

--- spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -23,5 +23,14 @@ public class TrackCoach implements Coach {
 		// TODO Auto-generated method stub
 		return "Just do it: " + fortuneService.getFortune();
 	}
-
+	
+	// add an init method
+	public void doMyStarupStuff() {
+		System.out.println("TrackCoach: inside method doMyStarupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyCleanupStuff() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuff");
+	}
 }
