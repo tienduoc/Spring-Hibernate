2f5b296d1642db6ac768f178da555f035608700b
bl@ck_c@t <tienduocict@gmail.com>
2018-04-15 15:01:49 +0700

    077-078: Spring Annotation - Bean Scope, @Scope

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+	public static void main(String[] args) {
+
+		// load spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+		
+		// check if they are the same bean
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the result
+		System.out.println("\nPointing to the same object: " + result);
+		System.out.println("\nMemory location for theCoach: " + theCoach);
+		System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+		
+		// close the context
+		context.close();
+		
+	}
+
+}
--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -2,9 +2,11 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach") // if not declare bean id, default bean id will be tennisCoach
+@Component // if not declare bean id, default bean id will be tennisCoach
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
