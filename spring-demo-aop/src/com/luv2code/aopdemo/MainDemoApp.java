e4c91008f4f1f699cc83a6be0f17da351115d4ea
bl@ck_c@t <tienduocict@gmail.com>
2018-04-29 01:55:33 +0700

    325-331: AOP Pointcut Expressions - Match Methods and Return Types

--- spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -3,21 +3,24 @@ package com.luv2code.aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 
 public class MainDemoApp {
 
 	public static void main(String[] args) {
 
+		// read spring config java class
 		AnnotationConfigApplicationContext context =  new AnnotationConfigApplicationContext(DemoConfig.class);
 		
+		// get bean from spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
 		
+		// call the business method
 		theAccountDAO.addAccount();
+		theMembershipDAO.addSillyMember();
 		
-		System.out.println("\nlet's call it again\n");
-		
-		theAccountDAO.addAccount();
-		
+		// close the context
 		context.close();
 	}
 
--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-	@Before("execution(public void addAccount())")
+//	@Before("execution(public void addAccount())")
+//	@Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
+//	@Before("execution(public void add*())")
+	@Before("execution(* add*())")
 	public void beforeAddAccountAdvice() {
 		System.out.println(">> executing @Before advice on addAccount()");
 	}
