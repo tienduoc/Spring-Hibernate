bf16a02b51539e5b17dddbcbb5584230ae89af3c
bl@ck_c@t <tienduocict@gmail.com>
2018-04-29 17:02:29 +0700

    377-383: AOP Logging to Spring MVC

---
+++ web-customer-tracker-aop/WebContent/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,57 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>List Customers</title>
+
+<!-- reference stylesheet -->
+<link type="text/css" rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/style.css">
+
+
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">
+			<h2>CRM - Customer Relationship Manager</h2>
+		</div>
+	</div>
+
+	<div id="container">
+		<div id="content">
+			<input type="button" value="Add Customer" onClick="window.location.href='showFormForAdd'; return false;"
+				class="add-button"
+			/>
+		
+			<table>
+				<tr>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Email</th>
+					<th>Action</th>
+				</tr>
+				<c:forEach var="tempCustomer" items="${customers}">
+					<!-- constuct an update link with customer id -->
+					<c:url var="updateLink" value="/customer/showFormForUpdate">
+						<c:param name="customerId" value="${tempCustomer.id}" />
+					</c:url>
+					<c:url var="deleteLink" value="/customer/delete">
+						<c:param name="customerId" value="${tempCustomer.id}" />
+					</c:url>
+					
+					<tr>
+						<td>${tempCustomer.firstName}</td>
+						<td>${tempCustomer.lastName}</td>
+						<td>${tempCustomer.email}</td>
+						<td><a href="${updateLink}">Update</a>|
+						<a href="${deleteLink}" 
+						onClick="if(!(confirm('Are you sure you want to delete this customer?'))) return false">Delete</a></td>
+					</tr>
+				</c:forEach>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
---
+++ web-customer-tracker-aop/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,75 @@
+package com.luv2code.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	// inject the customer service
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/list")
+	public String listCustomers(Model model) {
+		
+		// get customers from the dao
+		List<Customer> theCustomers = customerService.getCustomers();
+		
+		// add customers to the model
+		model.addAttribute("customers", theCustomers);
+		
+		return "list-customers";
+ 	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Customer theCustomer = new Customer();
+		theModel.addAttribute("customer", theCustomer);
+		
+		
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		// save the customer using our service
+		customerService.saveCustomer(theCustomer);
+		
+		return "redirect:/customer/list";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int theId, Model theModel) {
+		
+		// get the customer form service
+		Customer theCustomer = customerService.getCustomers(theId);
+		
+		// set customer as a model attribute to pre-populate the form
+		theModel.addAttribute("customer", theCustomer);
+		
+		// send over to our form
+		return "customer-form";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("customerId") int theId) {
+		customerService.deleteCustomer(theId);
+		return "redirect:/customer/list";
+	}
+}
