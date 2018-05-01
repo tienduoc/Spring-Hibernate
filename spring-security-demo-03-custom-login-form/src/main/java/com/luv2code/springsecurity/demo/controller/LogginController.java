968102dec2ae6e6ce221fc2fedfb4c7eb47c1544
bl@ck_c@t <tienduocict@gmail.com>
2018-05-01 08:31:51 +0700

    417-426: Spring Security - Custom Login Form

---
+++ spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/controller/LogginController.java
@@ -0,0 +1,14 @@
+package com.luv2code.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogginController {
+
+	@GetMapping("/showMyLoginPage")
+	public String showMyLogginPage() {
+		
+		return "plain-login";
+	}
+}
